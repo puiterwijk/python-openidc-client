5456800bb2e4b54a7a36c9749cf5614079cc88b3
Mohan Boddu <mboddu@redhat.com>
2017-03-23 12:00:36 -0400

    Skip tests on setup.py install
    Signed-off-by: Mohan Boddu <mboddu@redhat.com>

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     license='MIT',
     keywords='OpenID Connect Client',
     url='https://github.com/puiterwijk/python-openidc-client',
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     install_requires=[
         'requests',
